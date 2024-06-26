2bf8c3801588a4e2253f7e7f7a94ff1a6d61fb4b
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-06-26 14:53:51 +0800

    write a function to play 1 round and determine the winner of each round

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function getHumanChoice(){
     humanChoice = prompt("Enter Rock, Paper, or Scissors");
     return (humanChoice)   ;
 }
+
+
+
+
+let humanScore = 0 ; 
+let computerScore = 0 ;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if ((humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")){
+            console.log("You Win!");
+            humanScore++;
+        } else if (humanChoice == computerChoice){
+            console.log("It's a tie!")
+        } else if (humanChoice == "scissors" && computerChoice == "rock"){
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        } else if (humanChoice == "rock" && computerChoice == "paper"){
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        } else if (humanChoice == "paper" && computerChoice == "scissors"){
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        } else {
+            console.log("invalid choice! Enter the right format");
+        }
+
+}
