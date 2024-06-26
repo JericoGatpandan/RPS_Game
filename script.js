508bce073323f5d2c5b73213137830282a2d4aec
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-06-26 14:50:37 +0800

    Add a basic html structure and make a function to generate the computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+//Rock-Paper-Scissors Game
+
+//generate computer choice
+function getComputerChoice(){
+    const randNum = Math.random()*1;
+    if (randNum < 0.33) {
+        return "rock";
+    } else if (randNum < 0.66){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
