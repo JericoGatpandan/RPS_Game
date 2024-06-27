794111945f0d36e2f36475fb79d67628bfe5ce1d
JericoGatpandan <jericogatpandan0905@gmail.com>
2024-06-27 10:13:21 +0800

    add text foor accesibility on ui

--- script.js
+++ script.js
@@ -7,105 +7,86 @@ const body = document.body;
 const humanChoiceImg = document.getElementById("human-choice-img");
 const computerChoiceImg = document.getElementById("computer-choice-img");
 
-
 //generate computer choice
-function getComputerChoice(){
-    const randNum = Math.random()*1;
-    if (randNum < 0.33) {
-        return "rock";
-    } else if (randNum < 0.66){
-        return "paper";
-    } else {
-        return "scissors";
-    }
+function getComputerChoice() {
+  const randNum = Math.random() * 1;
+  if (randNum < 0.33) {
+    return "rock";
+  } else if (randNum < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-
-rockbtn.addEventListener("click", function(){
-    playRound("rock", getComputerChoice());
+rockbtn.addEventListener("click", function () {
+  playRound("rock", getComputerChoice());
 });
-paperbtn.addEventListener("click", function(){
-    playRound("paper", getComputerChoice());
+paperbtn.addEventListener("click", function () {
+  playRound("paper", getComputerChoice());
 });
-scissorsbtn.addEventListener("click", function(){
-    playRound("scissors", getComputerChoice());
+scissorsbtn.addEventListener("click", function () {
+  playRound("scissors", getComputerChoice());
 });
 
+let humanScore = 0;
+let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+  const choices = {
+    rock: "/assets/fist.png",
+    paper: "/assets/hand-paper.png",
+    scissors: "/assets/scissors.png",
+  };
 
+  humanChoiceImg.src = choices[humanChoice];
+  computerChoiceImg.src = choices[computerChoice];
 
-let humanScore = 0 ; 
-let computerScore = 0 ;
+  const win = document.querySelector("p");
+  const hScore = document.getElementById("hScore");
+  const cScore = document.getElementById("cScore");
 
-function playRound(humanChoice, computerChoice){
-    const choices = {
-        rock: '/assets/fist.png',
-        paper: '/assets/hand-paper.png',
-        scissors: '/assets/scissors.png'
-    };
+  if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    win.textContent = "You Win!";
+    win.style.color = "#F9F871";
+    body.append(win);
+    humanScore++;
+  } else if (humanChoice == computerChoice) {
+    win.textContent = "It's a tie!";
+    body.append(win);
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    win.textContent = "You lose! Rock beats Scissors";
+    win.style.color = "red";
+    body.append(win);
+    computerScore++;
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    win.textContent = "You lose! Paper beats Rock";
+    win.style.color = "red";
+    body.append(win);
+    computerScore++;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    win.textContent = "You lose! Scissors beats Paper";
+    win.style.color = "red";
 
-    humanChoiceImg.src = choices[humanChoice];
-    computerChoiceImg.src = choices[computerChoice];
+    body.append(win);
+    computerScore++;
+  } else {
+    console.log("invalid choice! Enter the right format");
+  }
 
-    const win = document.querySelector("p");
-    const hScore = document.getElementById("hScore");
-    const cScore = document.getElementById("cScore")
-    
-    if ((humanChoice == "rock" && computerChoice == "scissors") ||
-        (humanChoice == "paper" && computerChoice == "rock") ||
-        (humanChoice == "scissors" && computerChoice == "paper")){
-
-            win.textContent = "You Win!";
-            body.append(win);
-            humanScore++;
-        } else if (humanChoice == computerChoice){
-            win.textContent = "It's a tie!" ;
-            body.append(win)
-        } else if (humanChoice == "scissors" && computerChoice == "rock"){
-            win.textContent = "You lose! Rock beats Scissors";
-            body.append(win);
-            computerScore++;
-        const score = document.querySelector("h4");
-
-        } else if (humanChoice == "rock" && computerChoice == "paper"){
-            win.textContent = "You lose! Paper beats Rock";
-            body.append(win);
-            computerScore++;
-
-
-        } else if (humanChoice == "paper" && computerChoice == "scissors"){
-            win.textContent = "You lose! Scissors beats Paper";
-            body.append(win);
-            computerScore++;
-
-
-        } else {
-            console.log("invalid choice! Enter the right format");
-        }
-     
-       hScore.textContent = `Human Score: ${humanScore}`;
-       cScore.textContent = `Computer Score: ${computerScore}`
-       win.style.display = "flex";
-       win.style.justifyContent = "center";
-
-       
-
+  hScore.textContent = `Human Score: ${humanScore}`;
+  cScore.textContent = `Computer Score: ${computerScore}`;
+  win.style.display = "flex";
+  win.style.justifyContent = "center";
 }
-
-
 
 // for (i = 0; i < 5; i++){
 //     const humanSelection = getHumanChoice();
 //     const computerSelection = getComputerChoice();
-    
+
 //     playRound(humanSelection, computerSelection);
 //     }
-    
-   
-       
-       
-   
-    
-    
-    
-    
